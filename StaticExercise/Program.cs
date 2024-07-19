178993bdeeaeb8df8b2eccecf03f16b47a3cd654
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-19 10:27:19 -0500

    Exercise 1 Complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace StaticExercise
+﻿using System;
+namespace StaticExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"72° Fahrenheit is  {TempConverter.FahrenheitToCelsius(72):0.0}° Celsius");
+            Console.WriteLine($"31° Celsius is {TempConverter.CelsiusToFahrenheit(31):0.0}° Fahrenheit");
         }
     }
 }
